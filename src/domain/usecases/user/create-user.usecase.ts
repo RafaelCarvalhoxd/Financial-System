27e7116f6e73f371df00446ff59a29e7142b7c34
Faelzxd <devrafaelcarvalho14@gmail.com>
2024-06-05 19:02:59 -0300

    add create user usecase

---
+++ src/domain/usecases/user/create-user.usecase.ts
@@ -0,0 +1,22 @@
+import { Role } from '@/domain/entities/user/role.entity';
+import { User } from '@/domain/entities/user/user.entity';
+
+export interface CreateUserUseCase {
+  perform(params: CreateUserUseCase.Params): Promise<CreateUserUseCase.Result>;
+}
+
+export namespace CreateUserUseCase {
+  export type Params = {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    cpf: string;
+    birthdate: Date;
+    rg: string;
+    phone: string;
+    address: string;
+    role: Role;
+  };
+  export type Result = User;
+}
