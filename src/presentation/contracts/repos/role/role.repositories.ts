ce72cf3c93f608afe72f201e1d5072e5daee27b7
Faelzxd <devrafaelcarvalho14@gmail.com>
2024-06-06 10:59:07 -0300

    add role repositories

---
+++ src/presentation/contracts/repos/role/role.repositories.ts
@@ -0,0 +1,81 @@
+import { Role } from '@/domain/entities/role/role.entity';
+
+export interface ListRoleRepository {
+  listRoles: (
+    params: ListRoleRepository.Params,
+  ) => Promise<ListRoleRepository.Result>;
+}
+
+export namespace ListRoleRepository {
+  export type Params = {
+    name?: string;
+  };
+  export type Result = Role[];
+}
+
+export interface ShowRoleRepository {
+  showRole: (
+    params: ShowRoleRepository.Params,
+  ) => Promise<ShowRoleRepository.Result>;
+}
+
+export namespace ShowRoleRepository {
+  export type Params = {
+    id: string;
+  };
+  export type Result = Role | null;
+}
+
+export interface CreateRoleRepository {
+  createRole: (
+    params: CreateRoleRepository.Params,
+  ) => Promise<CreateRoleRepository.Result>;
+}
+
+export namespace CreateRoleRepository {
+  export type Params = {
+    id: string;
+    name: string;
+  };
+  export type Result = Role;
+}
+
+export interface UpdateRoleRepository {
+  updateRole: (
+    params: UpdateRoleRepository.Params,
+  ) => Promise<UpdateRoleRepository.Result>;
+}
+
+export namespace UpdateRoleRepository {
+  export type Params = {
+    id: string;
+    name: string;
+  };
+  export type Result = Role;
+}
+
+export interface DeleteRoleRepository {
+  deleteRole: (
+    params: DeleteRoleRepository.Params,
+  ) => Promise<DeleteRoleRepository.Result>;
+}
+
+export namespace DeleteRoleRepository {
+  export type Params = {
+    id: string;
+  };
+  export type Result = boolean;
+}
+
+export interface FindRoleByNameRepository {
+  findRole: (
+    params: FindRoleByNameRepository.Params,
+  ) => Promise<FindRoleByNameRepository.Result>;
+}
+
+export namespace FindRoleByNameRepository {
+  export type Params = {
+    name: string;
+  };
+  export type Result = Role | null;
+}
